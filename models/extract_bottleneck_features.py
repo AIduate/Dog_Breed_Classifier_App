7e0c0deabfd26fda72c65ad5d499e24085c8f549
Amanda <33467922+AIduate@users.noreply.github.com>
2022-02-27 04:47:40 -0500

    Update extract_bottleneck_features.py

--- models/extract_bottleneck_features.py
+++ models/extract_bottleneck_features.py
@@ -1,3 +1,3 @@
 def extract_Resnet50(tensor):
-	from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+	from keras.applications.resnet50 import ResNet50, preprocess_input
+	return ResNet50(weights='imagenet',input_shape=(224,224,3), pooling='avg', include_top=False).predict(preprocess_input(tensor))
